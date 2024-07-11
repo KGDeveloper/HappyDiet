806a10eb8ec4233b8dc6645b71c0c3d5591112b6
祁增奎 <qizengkui@clxkj.cn>
2024-07-11 18:37:56 +0800

    初始化工程

---
+++ HappyDiet/HappyDietApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyDietApp.swift
+//  HappyDiet
+//
+//  Created by MrQi on 2024/7/11.
+//
+
+import SwiftUI
+
+@main
+struct HappyDietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
